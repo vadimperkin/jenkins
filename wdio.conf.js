97bc244889a05376c90e4450595e65ed8cf7cb8d
Vadim <vadym.perkin@altran.com>
2023-08-12 14:26:27 +0400

    standalonne 123

--- wdio.conf.js
+++ wdio.conf.js
@@ -117,9 +117,7 @@ export const config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [["chromedriver", {
-    chromedriverCustomPath: `./node_modules/chromedriver/lib/chromedriver/chromedriver`
-  }]],
+  services: [],
   dockerOptions: {
     image: 'selenium/standalone-chrome',
     healthCheck: 'http://localhost:4444',
